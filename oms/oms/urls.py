3802df472716e1bede4ce0d78578bb9b4d5864c7
Divya Shrestha <divyashrestha1222gmail.com>
2026-05-11 15:20:48 +0100

    mapping /test/route in project

--- oms/oms/urls.py
+++ oms/oms/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from oms.quizhub.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("test/", hello_world, name="hello_world"),
 ]
