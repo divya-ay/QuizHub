0e441a5454e16224ea98475b01d48dfcf3e8e1d5
Divya Shrestha <divyashrestha1222@gmail.com>
2026-05-14 18:44:59 +0100

    installed bootstrap via npm and added as a dependency(30b)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -d "node_modules" ]; then
+npm install
+fi
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
