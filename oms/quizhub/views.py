de39cdadc0ccd138deefa5caf0f70958351608d5
Divya Shrestha <divyashrestha1222@gmail.com>
2026-05-14 18:14:51 +0100

    created main page in quizhub and inherited from base.html

--- oms/quizhub/views.py
+++ oms/quizhub/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import View
 
 def hello_world(request):
     return HttpResponse("Hello, world!")
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'quizhub/home.html')
