750dce71af70745d2a5c005a2313244491628b0f
Divya Shrestha <divyashrestha1222@gmail.com>
2026-06-20 00:11:31 +0100

    added some styles to login page

--- oms/userauth/views.py
+++ oms/userauth/views.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
 class CustomLoginView(LoginView):
     template_name = "userauth/login.html"
     redirect_authenticated_user = True
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field in form.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control form-control-lg",
+                "placeholder": field.label,
+            })
+        return form
