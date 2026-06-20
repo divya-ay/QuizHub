e7916f030973e6a21e0ba7ccd75dd65ff846e481
Divya Shrestha <divyashrestha1222@gmail.com>
2026-06-20 17:23:36 +0100

    signup page created

--- oms/userauth/urls.py
+++ oms/userauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import CustomLoginView
+from .views import CustomLoginView, SignUpView
 
 app_name = "userauth"
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
+    path("signup/", SignUpView.as_view(), name="signup"),
     path("logout/", LogoutView.as_view(next_page="quizhub:home"), name="logout"),
 ]
--- oms/userauth/views.py
+++ oms/userauth/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 class CustomAuthForm(AuthenticationForm):
     error_messages = {
@@ -20,3 +23,30 @@ class CustomLoginView(LoginView):
                 "placeholder": field.label,
             })
         return form
+
+class CustomSignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+        labels = {
+            "username": "Username",
+            "password1": "Password",
+            "password2": "Confirm Password",
+        }
+
+class SignUpView(CreateView):
+    form_class = CustomSignUpForm
+    template_name = "userauth/signup.html"
+    success_url = reverse_lazy("userauth:login")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field in form.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control form-control-lg",
+                "placeholder": field.label,
+            })
+        return form
+
+    def form_valid(self, form):
+        return super().form_valid(form)
